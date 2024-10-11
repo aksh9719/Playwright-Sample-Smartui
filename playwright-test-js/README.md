3308ef06cba82b5681eb6aaa65cc7f2d1a6dc6f5
akash9719 <gautamankush265@gmail.com>
2024-10-11 19:25:25 +0530

    smartui build

---
+++ playwright-test-js/README.md
@@ -0,0 +1,15 @@
+# Testing Playwright with JS on LambdaTest
+
+## Setup
+* Clone the repo. Run `cd playwright-test-js`
+* Install dependencies. Run `npm install`
+
+## Running your tests
+- To run a single test, run 
+  ```npm run test```
+## Running your smartui tests
+- To run smartui local, run
+```npx smartui exec  playwright test --config .smartui.json```
+- To run smartui local parallel
+- use *.specs.js in TestDir in playwright.config.js
+ 
---
+++ playwright-test-js/tests/test_1.spec.js
@@ -0,0 +1,26 @@
+const { test } = require('../lambdatest-setup');
+const { expect } = require('@playwright/test');
+const smartuiSnapshot = require("@lambdatest/playwright-driver");
+
+test.describe('Browse Lufthansa in different search engines', () => {
+  test('Search Lufthansa', async ({ page }) => {
+    try {
+      await page.goto('https://www.lufthansa.com/in/en/homepage');
+      let element = await page.locator("//*[@id=\"cm-acceptAll\"]");
+      await element.click();
+      await page.waitForTimeout(10000);
+      await smartuiSnapshot.smartuiSnapshot(page, "LT1");
+
+      await page.evaluate(() => {
+            window.scrollBy(0, 500); // Scroll down 500 pixels
+          });
+      let element2 = await page.locator('//*[@class="overlay-header-text display-alignment" and text()="Check-in"]');
+      await element2.click();
+      await page.waitForTimeout(10000);
+      await smartuiSnapshot.smartuiSnapshot(page, "LT");
+    } catch (error) {
+      console.error("An error occurred:", error);
+      // Optionally, you can handle specific error cases here
+    }
+  });
+});
